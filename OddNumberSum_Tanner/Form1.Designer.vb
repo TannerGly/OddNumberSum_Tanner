c3897d558483f2e39cdd42c0138b0e5b6c517183
TannerGly <tannerg@pagosa.k12.co.us>
2014-12-02 11:09:55 -0700

    Added Files

---
+++ OddNumberSum_Tanner/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblSum = New System.Windows.Forms.Label()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.btnSum = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a number:"
+        '
+        'lblSum
+        '
+        Me.lblSum.AutoSize = True
+        Me.lblSum.Location = New System.Drawing.Point(13, 168)
+        Me.lblSum.Name = "lblSum"
+        Me.lblSum.Size = New System.Drawing.Size(0, 13)
+        Me.lblSum.TabIndex = 1
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(101, 32)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumber.TabIndex = 2
+        '
+        'btnSum
+        '
+        Me.btnSum.Location = New System.Drawing.Point(72, 125)
+        Me.btnSum.Name = "btnSum"
+        Me.btnSum.Size = New System.Drawing.Size(75, 23)
+        Me.btnSum.TabIndex = 3
+        Me.btnSum.Text = "Find Sum of Odds"
+        Me.btnSum.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnSum)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.lblSum)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblSum As System.Windows.Forms.Label
+    Friend WithEvents txtNumber As System.Windows.Forms.TextBox
+    Friend WithEvents btnSum As System.Windows.Forms.Button
+
+End Class
---
+++ OddNumberSum_Tanner/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub btnSum_Click(sender As Object, e As EventArgs) Handles btnSum.Click
+        Dim lngSum As Long = 0
+        Dim intNumber As Integer = Val(Me.txtNumber.Text)
+
+        Dim intC As Integer
+        For intC = 1 To intNumber
+            If intC Mod 2 <> 0 Then
+                lngSum = lngSum + intC
+            End If
+        Next intC
+        Me.lblSum.Text = "The sum of the numbers between 1 and " & intNumber & " is " & lngSum
+    End Sub
+    Private Sub btnSum_TextChanged(sender As Object, e As EventArgs) Handles btnSum.TextChanged
+        Me.lblSum.Text = Nothing
+    End Sub
+End Class
